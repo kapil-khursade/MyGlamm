bd8aa37de30cefa3399f5e715ed77b7643f02053
NitinGautam09 <hinitingautam@gmail.com>
2022-08-25 23:02:20 +0530

    pushing whole day 3 data

--- productDetail.js
+++ productDetail.js
@@ -1,14 +1,25 @@
 // Will provide all the js required for detail.html
 let productDetailLS=JSON.parse(localStorage.getItem("productDetail")) || [];
+let recentPage= document.getElementById("recent_page")
+
+let currentPage= document.getElementById("current_page")
+
 displayDetails(productDetailLS)
 function displayDetails(data)
 {
-   
+  
+    recentPage.innerText=data.product_type;
+    localStorage.setItem("query",JSON.stringify(data.product_type));
+    currentPage.innerText=data.name;
+
+    
+
+
     let img = document.createElement('img');
     img.src=data.api_featured_image;
 
    
-    let title = document.createElement('h3');
+    let title = document.createElement('h1');
     title.innerText=data.name;
     let desc = document.createElement('p');
     desc.setAttribute("id","Pro_desc")
@@ -21,12 +32,15 @@ function displayDetails(data)
             // maindiv.style.cursor="not-allowed";
         }
         else{
-            price.innerText=`$${data.price}`;
+            price.innerText=`₹${Math.floor(+(data.price)*30)}`;
         }
 
         document.getElementById("rightBox").append(title,desc,price)
         document.getElementById("leftBox").append(img)
-      
-   
 }
+
+
+recentPage.addEventListener("click",function(){
+    window.location.href="./product.html";
+})
 
